04e2b954339b2cf3ab50e54c474090b7ccafd2b7
JinwangMok <wlsdhkd8040@gmail.com>
2022-12-06 17:49:12 +0900

    Calculate Focus Point

--- PRETI.cpp
+++ PRETI.cpp
@@ -3,6 +3,9 @@
 
 #define FACE_STOREGE_SIZE 30
 #define FACE_ERROR_MARGIN 30
+#define FACE_MIN_NEIGHBORS 9
+#define EYES_MIN_NEIGHBORS 15
+
 
 using namespace std;
 using namespace cv;
@@ -26,7 +29,7 @@ int main(){
     vector<Rect> eyesFromLeft, eyesFromRight;
     queue<Rect> latest_faces; // index -1 : latest face rect
     uint16_t face_error_count = 0;
-    Point leftCenter, rightCenter;
+    Point leftCenter, rightCenter, focusPoint;
 
     //TODO: 얼굴 검출 안된 경우 예외 처리(지금은 강제 종료됨)
     while(true){
@@ -39,7 +42,7 @@ int main(){
         flip(frame, frame, 1);                                          // 좌우반전
         cvtColor(frame, gray_frame, COLOR_BGR2GRAY);                    // 계산을 위해 그레이 스케일로 변환
 
-        face_classifier.detectMultiScale(gray_frame, faces, 1.1, 9);     // scaleFactor=1.1, minNeighbors=9
+        face_classifier.detectMultiScale(gray_frame, faces, 1.1, FACE_MIN_NEIGHBORS);     // scaleFactor=1.1, minNeighbors=9
 
         /* 얼굴 선택기(예외 발생 시 가장 최근 선택된 얼굴로 강제 대체) */
         // TODO: 프레임 간의 얼굴 영역 마진 처리
@@ -75,19 +78,26 @@ int main(){
         leftEyeROI = frame(Rect(faceROI.x, faceROI.y, cvRound(faceROI.width/2), faceROI.height));
         rightEyeROI = frame(Rect(faceROI.x+cvRound(faceROI.width/2), faceROI.y, cvRound(faceROI.width/2), faceROI.height));
         
-        eye_classifier.detectMultiScale(leftEyeROI, eyesFromLeft, 1.1, 8); // scaleFactor=1.1, minNeighbors=8
-        eye_classifier.detectMultiScale(rightEyeROI, eyesFromRight, 1.1, 8); // scaleFactor=1.1, minNeighbors=8
+        eye_classifier.detectMultiScale(leftEyeROI, eyesFromLeft, 1.1, EYES_MIN_NEIGHBORS); // scaleFactor=1.1
+        eye_classifier.detectMultiScale(rightEyeROI, eyesFromRight, 1.1, EYES_MIN_NEIGHBORS); // scaleFactor=1.1
         
-        // TODO: 안구 검출 안되는 경우(ex. 눈 감을 때) 예외 처리
+        // TODO: 안구 검출 안되는 경우(ex. 눈 감을 때) 예외 처리. PPT 알고리즘 5번 참고
         for (Rect eye : eyesFromLeft) {
-            Point center(eye.x + eye.width / 2, eye.y + eye.height / 2);
-            circle(leftEyeROI, center, eye.width / 2, Scalar(255, 0, 0), 2, LINE_AA);
+            leftCenter = Point(eye.x + eye.width / 2, eye.y + eye.height / 2);
+            circle(frame(faceROI), leftCenter, 3, Scalar(255, 0, 0), -1, LINE_AA);
         }
 
         for (Rect eye : eyesFromRight) {
-            Point center(eye.x + eye.width / 2, eye.y + eye.height / 2);
-            circle(rightEyeROI, center, eye.width / 2, Scalar(255, 0, 0), 2, LINE_AA);
+            rightCenter = Point((eye.x + eye.width / 2)+leftEyeROI.cols, eye.y + eye.height / 2);
+            circle(frame(faceROI), rightCenter, 3, Scalar(255, 0, 0), -1, LINE_AA);
         }
+
+        focusPoint = Point(((rightCenter.x-leftCenter.x)/2)+leftCenter.x, // ((r-l)/2) + l
+                            leftCenter.y<rightCenter.y?((leftCenter.y-rightCenter.y)/2)+leftCenter.y:((rightCenter.y-leftCenter.y)/2)+rightCenter.y); 
+        
+        circle(frame(faceROI), focusPoint, 3, Scalar(0, 255, 255), -1, LINE_AA);
+
+        line(frame(faceROI), leftCenter, rightCenter, Scalar(0, 0, 255), 1, LINE_AA);
 
         imshow("frame", frame);
 
